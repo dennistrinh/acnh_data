07fd2aba43e13341d5208ec4b8a1b85ffcf0bffe
EC2 Default User <ec2-user@ip-172-31-84-173.ec2.internal>
2020-05-15 01:19:29 +0000

    Added filtering

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const dotenv = require('dotenv').config();
 const mariadb = require('./config.js');
 const port = 9000;
+const monthToDate = require('./monthToDate.js');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,7 +17,7 @@ app.get('/', function(req, res) {
 });
 
 app.get('/bugs', async (req, res) => {
-	let sql = 'SELECT name, price FROM bugs'
+	const sql = 'SELECT name, location, price FROM bugs'
 	let conn;
 	try {
 		conn = await mariadb.pool.getConnection();
@@ -29,8 +30,24 @@ app.get('/bugs', async (req, res) => {
 	}
 });
 
+app.post('/bugs', async(req, res) => {
+	const sql = 'SELECT name, location, price FROM bugs WHERE (start_month_1=\"2020';
+	const date = monthToDate(req.body["month"]);
+	const sqlQuery = sql.concat(date, '\" OR start_month_2=\"2020', date, '\")');
+	let conn;
+	try {
+		conn = await mariadb.pool.getConnection();
+		let rows = await conn.query(sqlQuery);
+		res.render('./filters/bugsFilter', {data: rows, body_month: req.body["month"]});
+	} catch(err) {
+		throw err;
+	} finally {
+		if (conn) return conn.release();
+	}
+});
+
 app.get('/fish', async (req, res) => {
-	let sql = 'SELECT name, price FROM fish'
+	const sql = 'SELECT name, location, price FROM fish'
 	let conn;
 	try {
 		conn = await mariadb.pool.getConnection();
@@ -43,6 +60,21 @@ app.get('/fish', async (req, res) => {
 	}
 });
 
+app.post('/fish', async(req, res) => {
+	const sql = 'SELECT name, location, price FROM fish WHERE (start_month_1=\"2020';
+	const date = monthToDate(req.body["month"]);
+	const sqlQuery = sql.concat(date, '\" OR start_month_2=\"2020', date, '\")');
+	let conn;
+	try {
+		conn = await mariadb.pool.getConnection();
+		let rows = await conn.query(sqlQuery);
+		res.render('./filters/fishFilter', {data: rows, body_month: req.body["month"]});
+	} catch(err) {
+		throw err;
+	} finally {
+		if (conn) return conn.release();
+	}
+});
 
 var server = app.listen(port, function() {
 	console.log('Express started on localhost:' + port);
